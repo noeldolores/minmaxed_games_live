40d188f1163f427c07b24b9f62e62f857c3a38de
noeldolores <noel.dolore@gmail.com>
2022-11-10 07:33:49 -0800

    Add new script for pulling specific server data

---
+++ update_specific_server.py
@@ -0,0 +1,170 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+import pytz
+from website import app, db, models
+import time
+from website.scripts.newworld import player_data
+
+
+def print_stderr(output=str):
+  try:
+    print(output)
+    return True
+  except Exception as e:
+    print(f"print_stderr: {e}")
+    return False
+
+
+def timer(time_history=None, function=None):
+  if not time_history:
+    base = time.time()
+    time_history = [('Beginning task...', base, 0)]
+    print_stderr(time_history[0])
+    return time_history
+
+  if function:
+    stamp = time.time()
+    lap_num = len(time_history) - 1
+    total_time = round(stamp - time_history[0][1], 4)
+    lap_time = round(total_time - time_history[lap_num][2], 4)
+    lap_data = ((str(function), lap_time, total_time))
+    time_history.append(lap_data)
+    print_stderr(lap_data)
+    return time_history
+
+
+def str_to_datetime(date_string):
+    _date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S.%f')
+    return _date.astimezone(pytz.utc)
+
+
+def datetime_to_str(date_time):
+    _local = pytz.utc.localize(date_time, is_dst=None).astimezone()
+    _date = datetime.strftime(_local, '%m/%d %I:%M %p')
+    return _date
+
+
+def request_server_data(stopwatch, server_name_num):
+    # Retrieve server name/id dictionary
+    server_dict = {}
+    server_name, api_id = server_name_num.lower().split(",")
+    server_dict[server_name] = {
+        'api_id' : api_id,
+        'items' : {}
+        }
+    
+    # Filter by used items
+    material_source = player_data.trade_post_order()
+    trophy_source = player_data.trade_post_trophy_order()
+    full_item_check_list = []
+    for material_list in material_source:
+        full_item_check_list.extend(material_list[1:])
+    for trophy_list in trophy_source:
+        category = trophy_list[0]
+        if category != "components":
+            for item in trophy_list[1:]:
+                if item in ['minor', 'basic', 'major']:
+                    full_item_check_list.append(f'{item}_{category}_trophy')
+                else:
+                    full_item_check_list.append(item)
+        else:
+            full_item_check_list.extend(trophy_list[1:])
+            
+    for server_name, server_data in server_dict.items():
+        api_id = server_data['api_id']
+        url = f"https://nwmarketprices.com/api/latest-prices/{api_id}/"
+        try:
+            my_timeout = 300
+            response = requests.request(method='GET', url=url, timeout=my_timeout)
+        except Exception as e:
+            print(response.status_code, e)
+            continue
+        
+        if response.status_code == 200:
+            stopwatch = timer(stopwatch, f'{server_name} : Response Success {response.status_code}')
+            
+            soup = BeautifulSoup(response.content, "html.parser")
+            item_list = json.loads(str(soup))
+            item_check_ref = {}
+            #for i in range(len(item_list)):
+            for item in item_list:
+                name = item['ItemName'].replace("'","").replace(" ","_").lower()
+                if name in full_item_check_list:
+                    item_check_ref[name] = {
+                        'ID': item['ItemId'],
+                        'Price' : item['Price'],
+                        'Availability' : item['Availability'],
+                        'LastUpdated' : str_to_datetime(item['LastUpdated']),
+                    }
+                    
+            dates_list=[]
+            for item, data in item_check_ref.items():
+                server_dict[server_name]['items'][item] = {
+                    'Name': item,
+                    'ID': data['ID'],
+                    'Price' : data['Price'],
+                    'Availability' : data['Availability'],
+                    'LastUpdated' : data['LastUpdated'],
+                }
+                    
+                dates_list.append(data['LastUpdated'])
+
+            if len(dates_list) > 0:
+                latest_date = max(dates_list)
+                server_dict[server_name]['latest_date'] = latest_date
+            else:
+                server_dict[server_name]['latest_date'] = None
+        else:
+            stopwatch = timer(stopwatch, f'{server_name} : Unable to connect. Response from server: {response.status_code}')
+            continue
+        
+    # Push data to db
+    for server_name, server_data in server_dict.items():
+        server = models.Market.query.filter_by(name=server_name).first()
+        
+        if server is None:
+            server = models.Market(name=server_name, server_id=server_data['api_id'])
+            db.session.add(server)
+            db.session.commit()
+            server = models.Market.query.filter_by(name=server_name).first()
+            
+        item_data = server_data['items']
+        if len(item_data) > 0:
+            for item, item_info in item_data.items():
+                item_check = models.Item.query.filter_by(market_id=server.server_id).filter_by(item_id=item_info['ID']).first()
+                if item_check:
+                    item_check.price = item_info['Price']
+                    item_check.availability = item_info['Availability']
+                    item_check.last_update = item_info['LastUpdated']
+                else:
+                    new_item = models.Item(last_update=item_info['LastUpdated'], item_id=item_info['ID'], name=item_info['Name'], price=item_info['Price'], availability=item_info['Availability'], market_id=server.id)
+                    db.session.add(new_item)
+            
+            latest_date = server_dict[server_name]['latest_date']
+            if latest_date:
+                server.last_update = latest_date
+                stopwatch = timer(stopwatch, f'{server_name} updated with {len(item_data)} items to {latest_date}')
+        else:
+            stopwatch = timer(stopwatch, f'{server_name} : Empty item_data dictionary')
+            
+        db.session.commit()
+    return True
+
+
+def main():
+    with app.app_context():
+        stopwatch = timer()
+        try:
+            server_name_num = "Delos,9"
+            full_pull = request_server_data(stopwatch, server_name_num)
+            print(full_pull)
+        except Exception as e:
+            full_pull = False
+            db.session.rollback()
+            print(e)
+        stopwatch = timer(stopwatch, f'Task completed...{full_pull}')
+    
+if __name__ == "__main__":
+  main()
