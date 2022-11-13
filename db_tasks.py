164cdcb4895cc05085f5aa1b96f33657b7cc98fb
noeldolores <noel.dolore@gmail.com>
2022-11-13 07:57:25 -0800

    Test time for full item pull: 3.2

--- db_tasks.py
+++ db_tasks.py
@@ -158,7 +158,7 @@ def request_server_data(stopwatch, server_name_num):
                 server.last_update = latest_date
                 #item_update_count = len(item_data)
                 update_percentage = round((item_update_count / total_item_count)*100,1)
-                stopwatch = timer(stopwatch, f'{server_name} : {update_percentage}% () to {datetime_to_str(latest_date)}')
+                stopwatch = timer(stopwatch, f'{server_name} : {update_percentage}% ({item_update_count}) to {datetime_to_str(latest_date)}')
         else:
             stopwatch = timer(stopwatch, f'{server_name} : Unable to connect.')
             
