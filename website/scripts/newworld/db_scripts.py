c47cca98dcf9a3bee7001ab73304bfffcd83d4cd
noeldolores <noel.dolore@gmail.com>
2022-11-13 13:51:57 -0800

    Crash related to market load: 3

--- website/scripts/newworld/db_scripts.py
+++ website/scripts/newworld/db_scripts.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pytz
 from ... import db
 from ...models import Market, Item
-
+import player_data
 
 
 def datetime_to_str(date_time):
@@ -16,6 +16,23 @@ def load_market_server(server_id):
     server = None
     market_dict = None
     tries = 0
+    
+    material_source = player_data.trade_post_order()
+    trophy_source = player_data.trade_post_trophy_order()
+    full_item_check_list = []
+    for material_list in material_source:
+        full_item_check_list.extend(material_list[1:])
+    for trophy_list in trophy_source:
+        category = trophy_list[0]
+        if category != "components":
+            for item in trophy_list[1:]:
+                if item in ['minor', 'basic', 'major']:
+                    full_item_check_list.append(f'{item}_{category}_trophy')
+                else:
+                    full_item_check_list.append(item)
+        else:
+            full_item_check_list.extend(trophy_list[1:])
+    
     try:
         while server is None:
             if tries > 10:
@@ -27,8 +44,9 @@ def load_market_server(server_id):
         if server: 
             market_dict = {}
             for item in server.items:
-                item_name = item.name
-                item_dict[item_name] = float(item.price)
+                if item in full_item_check_list:
+                    item_name = item.name
+                    item_dict[item_name] = float(item.price)
 
             market_dict['name'] = server.name
             market_dict['last_update'] = datetime_to_str(server.last_update)
