e248545eab6a9eca467d4e45be508a7e56390dfd
noeldolores <noel.dolore@gmail.com>
2022-12-21 18:33:34 -0800

    Fix fetch servers status check

--- db_tasks.py
+++ db_tasks.py
@@ -75,7 +75,7 @@ def request_server_data(stopwatch, server_name_num):
     for server_name, server_data in server_dict.items():
         status = server_status.get(server_name)
         if status:
-            age = (status[server_name]['nwmarketprices_update'] - status[server_name]['db_update']).seconds
+            age = (server_status[server_name]['nwmarketprices_update'] - server_status[server_name]['db_update']).seconds
             server_update[server_name] = {
                 'update': age > age_minimum,
                 'age': age
